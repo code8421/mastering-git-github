e4404b689dadf22225808698de1c8ffaa3883264
code8421 <znu3173@gmail.com>
2023-04-09 16:27:17 +0900

    Add test case

---
+++ test.spec.js
@@ -0,0 +1,10 @@
+// 'Default Test Set 이라는 식별자로 테스트 케이스 2개를 작성
+describe('Default Test Set', () => {
+    it('test1 should be passed', () => {
+        console.log('test1 passed');
+    });
+
+    it('test2 should be passed', () => {
+        console.log('test2 passed');
+    });
+});
